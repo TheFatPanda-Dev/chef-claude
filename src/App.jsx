99bbfee9e09dae41fbee3093fb1b8856481955e1
Afrim Avmedoski <afrim.avmedoski@gmail.com>
2025-09-08 15:34:52 +0200

    main form with partial design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
 	return (
 		<div className="flex flex-col items-center w-full">
 			<Header />
+			<Main />
 		</div>
 	);
 }
