fa29d02d0818569050db7dbc213cd799a6642003
Afrim Avmedoski <afrim.avmedoski@gmail.com>
2025-09-19 09:46:12 +0200

    added condition that shows get recipe only after 3+ ingridients are added

--- chef-claude/src/components/Form.jsx
+++ chef-claude/src/components/Form.jsx
@@ -85,22 +85,25 @@ export default function Form() {
 							</li>
 						))}
 					</ul>
-					<div className="flex justify-between items-center rounded-lg bg-[#F0EFEB] mt-10 px-7 py-7">
-						<div>
-							<h3 className="text-[1.125rem] font-medium leading-6 mb-2">
-								Ready for a recipe?
-							</h3>
-							<p className="text-gray-500 text-sm leading-5">
-								Generate a recipe from your list of ingredients.
-							</p>
+
+					{ingredients.length > 3 && (
+						<div className="flex justify-between items-center rounded-lg bg-[#F0EFEB] mt-10 px-7 py-7">
+							<div>
+								<h3 className="text-[1.125rem] font-medium leading-6 mb-2">
+									Ready for a recipe?
+								</h3>
+								<p className="text-gray-500 text-sm leading-5">
+									Generate a recipe from your list of ingredients.
+								</p>
+							</div>
+							<button
+								className="border-none rounded-md bg-[#D17557] shadow-sm text-[#FAFAF8] px-4 py-2 font-sans text-sm cursor-pointer"
+								type="button"
+							>
+								Get a recipe
+							</button>
 						</div>
-						<button
-							className="border-none rounded-md bg-[#D17557] shadow-sm text-[#FAFAF8] px-4 py-2 font-sans text-sm cursor-pointer"
-							type="button"
-						>
-							Get a recipe
-						</button>
-					</div>
+					)}
 				</div>
 			)}
 		</main>
