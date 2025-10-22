a5bdd62fe21011c4298a1815fe0b79ec97a48d63
Afrim Avmedoski <afrim.avmedoski@gmail.com>
2025-10-22 07:58:29 +0200

    tweaks

--- src/ai.js
+++ src/ai.js
@@ -1,7 +1,7 @@
 import Anthropic from "@anthropic-ai/sdk";
 
 const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in Markdown to make it easier to render to a web page
 `;
 
 // 🚨👉 ALERT: Read message below! You've been warned! 👈🚨
@@ -16,8 +16,6 @@ You are an assistant that receives a list of ingredients that a user has and sug
 // API keys private.
 
 const anthropic = new Anthropic({
-	// Make sure you set an environment variable in Scrimba
-	// for VITE_ANTHROPIC_API_KEY (note the VITE_ prefix)
 	apiKey: import.meta.env.VITE_ANTHROPIC_API_KEY,
 	dangerouslyAllowBrowser: true,
 });
